0c4b527153cf1b5069a8471a5c7d4e70ddcd096b
Zeovetar <zeovetar@gmail.com>
2021-10-27 19:44:20 +0300

    thinking about

--- fileManager/Program.cs
+++ fileManager/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Configuration;
 
 namespace fileManager
 {
@@ -8,11 +9,14 @@ namespace fileManager
     {
         static void Main(string[] args)
         {
-            List<string> listdisk = new List<string>();
             Console.OutputEncoding = System.Text.Encoding.Unicode;
             Console.InputEncoding = System.Text.Encoding.Unicode;
-            var PathToFolder = @"g:\";
-            string[] folders = Directory.GetDirectories(PathToFolder);
+            var @PathToFolder = folder("lastFolder");
+            int strCount = Int32.Parse(folder("strCount"));
+            List<string> listdisk = new List<string>();
+            data4draw(PathToFolder, strCount, listdisk);
+            drawtree(listdisk, strCount);
+ /*           string[] folders = Directory.GetDirectories(PathToFolder);
             int i = 0;
             listdisk.Add(PathToFolder);
             foreach (var lsUpper in folders)
@@ -35,47 +39,62 @@ namespace fileManager
                     }
                     }
                 i++;
-            }
-
-            i = 0;
-            foreach (var ls in listdisk)
-            {
-                Console.WriteLine(ls);
-                i++;
-                if (i == 29)
-                {
-                    return;
-                }
-            }
-            /*List<string> ls = GetRecursFiles(PathToFolder);
-            foreach (string fname in ls)
-            {
-                Console.WriteLine(fname);
-            }
-
-            List<string> GetRecursFiles(string start_path)
-            {
-                List<string> ls = new List<string>();
-                try
-                {
-                    string[] folders = Directory.GetDirectories(start_path);
-                    foreach (string folder in folders)
-                    {
-                        ls.Add("Папка: " + folder);
-                        ls.AddRange(GetRecursFiles(folder));
-                    }
-                    string[] files = Directory.GetFiles(start_path);
-                    foreach (string filename in files)
-                    {
-                        ls.Add("Файл: " + filename);
-                    }
-                }
-                catch (System.Exception e)
-                {
-                    Console.WriteLine(e.Message);
-                }
-                return ls;
             }*/
+
+            static void drawtree(List<string> listdisk, int strCount)
+            {
+                int i = 0;
+                foreach (var ls in listdisk)
+                {
+                    Console.WriteLine(ls);
+                    i++;
+                    if (i == strCount)
+                    {
+                        return;
+                    }
+                }
+            }
+
+        static string folder(string str)
+            {
+                var last = ConfigurationManager.AppSettings[str];
+                return last;
+            }
+
+
+        static void data4draw(string strmem, int count, List<string> listdisk)
+            {
+                string[] folders = Directory.GetDirectories(strmem);
+                string[] files = Directory.GetFiles(strmem);
+                //var listdata = new List<string>(folders.GetLength(0));
+                foreach (var i in folders)
+                    listdisk.Add(i);
+                foreach (var i in files)
+                    listdisk.Add(i);
+                /*                int i = 0;
+                                listdisk.Add(strmem);
+                                foreach (var lsUpper in folders)
+                                {
+                                    if (folders[i] != $"{strmem}System Volume Information")
+                                    {
+                                        string[] insfolders = Directory.GetDirectories(folders[i]);
+                                        string[] insfiles = Directory.GetFiles(folders[i]);
+                                        string[] str = folders[i].Split("\\");
+                                        listdisk.Add(str[1]);
+                                        foreach (var ls in insfolders)
+                                        {
+                                            string[] str1 = ls.Split("\\");
+                                            listdisk.Add($"|    {str1[2]}");
+                                        }
+                                        foreach (var ls in insfiles)
+                                        {
+                                            string[] str1 = ls.Split("\\");
+                                            listdisk.Add($"|    {str1[2]}");
+                                        }
+                                    }
+                                    i++;
+                                }*/
+            }
         }
     }
 }
